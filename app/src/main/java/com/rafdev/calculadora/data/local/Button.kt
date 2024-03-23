1898465caea58b59dce7f9da04363a475eed57ee
israe <israeltineo84@gmail.com>
2024-03-23 17:19:41 -0300

    add data for button

---
+++ app/src/main/java/com/rafdev/calculadora/data/local/Button.kt
@@ -0,0 +1,8 @@
+package com.rafdev.calculadora.data.local
+
+import com.rafdev.calculadora.data.local.response.ResponseDataButton
+
+interface Button {
+
+    fun getDataButton():ResponseDataButton
+}
